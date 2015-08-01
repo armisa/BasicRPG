903e867ae8d0d76fa84d658fd050544ff48eedaa
armisa <armisa.dev@gmail.com>
2015-08-01 13:37:43 -0400

    Basic functional screen
    Just a basic screen created and running with no errors

---
+++ desktop/src/com/armisa/basicRPG/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.armisa.basicRPG.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.armisa.basicRPG.BasicRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BasicRPG(), config);
+	}
+}
---
+++ html/src/com/armisa/basicRPG/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.armisa.basicRPG.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.armisa.basicRPG.BasicRPG;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new BasicRPG();
+        }
+}
